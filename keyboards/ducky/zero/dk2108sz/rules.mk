037333ad6738da16c090343c68226131f0292759
Husam Younis <youhusam@gmail.com>
2024-04-10 19:06:56 +0300

    fix keymaps

--- keyboards/ducky/zero/dk2108sz/rules.mk
+++ keyboards/ducky/zero/dk2108sz/rules.mk
@@ -28,5 +28,5 @@ NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-DIP_SWITCH_ENABLE = yes
+DIP_SWITCH_ENABLE = no
 
