aaf451af6bc1c33dc5a2688b1234616554133a2b
Husam Younis <youhusam@gmail.com>
2024-08-03 21:50:26 +0300

    Refactor encoder mode rendering and add display mode selection

--- keyboards/2040pad/keymaps/via/keymap.c
+++ keyboards/2040pad/keymaps/via/keymap.c
@@ -17,32 +17,33 @@
 #include QMK_KEYBOARD_H
 
 #include <helpers/helpers.c>
+#include <helpers/encoders.c>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         LT(1, LENC),   LT(2, RENC),
-        KC_KP_SLASH,   KC_KP_ASTERISK,   KC_KP_MINUS,   KC_KP_PLUS,
+        KC_KP_SLASH,   KC_KP_ASTERISK,   KC_KP_PLUS,   KC_KP_MINUS,
         KC_KP_7,   KC_KP_8,   KC_KP_9,   KC_KP_0,
         KC_KP_4,   KC_KP_5,   KC_KP_6,   KC_KP_ENTER,
         KC_KP_1,   KC_KP_2,   KC_KP_3,   KC_KP_DOT
     ),
     [1] = LAYOUT(
-        LT(3, LENC),   LT(3, RENC),
+        LT(3, LENC),   MODE_SELECT,
         QK_BOOT,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______
     ),
     [2] = LAYOUT(
-        LT(3, LENC),   LT(3, RENC),
+        MODE_SELECT,   LT(3, RENC),
         _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
-        _______,   _______,   _______,   _______
+        QK_CLEAR_EEPROM,   _______,   _______,   _______
     ),
     [3] = LAYOUT(
         LENC,   RENC,
-        _______,   _______,   _______,   _______,
+        MODE_SELECT,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
         QK_CLEAR_EEPROM ,   _______,   _______,   _______
@@ -59,27 +60,71 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
-
 #ifdef OLED_ENABLE
-#include <helpers/display.c>
+#    include <helpers/display.c>
 #endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(1, LENC):
-            if (record->event.pressed && record->tap.count == 1) {
-                register_code(KC_NUM_LOCK);
-            } else if (!record->event.pressed && record->tap.count == 1) {
-                unregister_code(KC_NUM_LOCK);
+        case LENC:
+            if (record->event.pressed && record->tap.count) {
+                if (display_mode == ENCODER_SELECT) {
+                    display_mode = NORMAL;
+                    return false;
+                }
+                left_encoder_pressed(record->event.pressed);
+                return false;
+            } else if (!record->event.pressed && record->tap.count) {
+                left_encoder_pressed(record->event.pressed);
+                return false;
             }
             break;
         case LT(2, RENC):
-            if (record->event.pressed && record->tap.count == 1) {
-                register_code(KC_MUTE);
-            } else if (!record->event.pressed && record->tap.count == 1) {
-                unregister_code(KC_MUTE);
+        case RENC:
+            if (record->event.pressed && record->tap.count) {
+                if (display_mode == ENCODER_SELECT) {
+                    display_mode = display_mode_selector;
+                    return false;
+                }
+                right_encoder_pressed(record->event.pressed);
+                return false;
+            } else if (!record->event.pressed && record->tap.count) {
+                right_encoder_pressed(record->event.pressed);
+                return false;
             }
             break;
+        case MODE_SELECT:
+            if (record->event.pressed) {
+                display_mode_selector = display_mode;
+                display_mode          = ENCODER_SELECT;
+            }
+            return false;
+            break;
+        default:
+            if (record->event.pressed) {
+                return display_mode != ENCODER_SELECT;
+            }
     }
     return true;
 }
+
+#ifdef ENCODER_ENABLE
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            left_encoder_cw();
+        } else {
+            left_encoder_ccw();
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            right_encoder_cw();
+        } else {
+            right_encoder_ccw();
+        }
+    }
+    return false;
+}
+#endif
