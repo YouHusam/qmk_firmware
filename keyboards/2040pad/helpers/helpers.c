2fc69644c74fdd6107df67832acb1cc7feb24b3a
Husam Younis <youhusam@gmail.com>
2024-08-03 21:50:26 +0300

    Refactor encoder mode rendering and add display mode selection

--- keyboards/2040pad/helpers/helpers.c
+++ keyboards/2040pad/helpers/helpers.c
@@ -3,7 +3,8 @@
 
 enum custom_keycodes {
     LENC = SAFE_RANGE,
-    RENC
+    RENC,
+    MODE_SELECT,
 };
 
 enum encoder_modes {
@@ -12,7 +13,31 @@ enum encoder_modes {
     TEXT,
     APPSW,
     MEDIA,
-    PONG
+    // PONG,
+    ENCODER_MODES_COUNT
+};
+
+const char* encoder_mode_names[] = {
+    "SCROLL",
+    "MOUSE",
+    "TEXT",
+    "APPSW",
+    "MEDIA",
+    // "PONG"
 };
 
 uint8_t enc_mode = SCROLL;
+
+enum display_modes {
+    NORMAL,
+    CALCULATOR,
+    ENCODER_SELECT
+};
+
+const char* display_mode_names[] = {
+    "NUMPAD",
+    "CALCULATOR"
+};
+
+uint8_t display_mode = NORMAL;
+uint8_t display_mode_selector = NORMAL;
