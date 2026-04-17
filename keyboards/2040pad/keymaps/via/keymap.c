62d45ed9e7eb4373d28752fca9972e297d9d2acb
Husam Younis <youhusam@gmail.com>
2026-04-17 22:17:39 +0300

    add reboot

--- keyboards/2040pad/keymaps/via/keymap.c
+++ keyboards/2040pad/keymaps/via/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
     [1] = LAYOUT(
-        QK_BOOT,   _______, LT(3, LENC),   MODE_SELECT,
+        QK_BOOT,   QK_RBT, LT(3, LENC),   MODE_SELECT,
         _______,
         _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
